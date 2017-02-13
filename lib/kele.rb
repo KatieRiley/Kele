2585ee86c9fc86b6acd2239f0fd5dde42df80466
Katie Riley <katieAnRiley@gmail.com>
2017-02-13 14:10:07 -0800

    added mentor avalibility

--- lib/kele.rb
+++ lib/kele.rb
@@ -9,7 +9,11 @@ class Kele
   end
 
   def get_me
-    response = self.class.get(bloc_base_api_url, headers: { "authorization" => @auth })
+    response = self.class.get(api_url('users/me'), headers: { "authorization" => @auth })
     @current_user = JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_avalability"), headers: { "authorization" => @auth})
   end
 end
